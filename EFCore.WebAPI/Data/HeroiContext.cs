5a1721b7abf18b023d877d9026578823218e23fc
Luiz Felipe D. Machado <95587655+DoniMachado@users.noreply.github.com>
2024-04-14 17:06:47 -0300

    initial commit
    estudo ef core

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,33 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data;
+
+public class HeroiContext: DbContext
+{
+    public DbSet<Heroi> Herois { get; set; }
+    public DbSet<Batalha> Batalhas { get; set; }
+    public DbSet<Arma> Armas { get; set; }
+    public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+    public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+   
+
+    public HeroiContext(DbContextOptions<HeroiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Heroi>().ToTable("Heroi");
+        modelBuilder.Entity<Batalha>().ToTable("Batalha");
+        modelBuilder.Entity<Arma>().ToTable("Arma");
+        modelBuilder.Entity<IdentidadeSecreta>().ToTable("IdentidadeSecreta");
+        modelBuilder.Entity<HeroiBatalha>().ToTable("HeroiBatalha");
+ 
+        modelBuilder.Entity<HeroiBatalha>(entity => entity.HasKey(e => new { e.BatalhaId,e.HeroiId}));
+    }
+
+
+
+
+}
---
+++ EFCore.WebAPI/Models/Heroi.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore.WebAPI.Models;
+
+public class Heroi
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public IdentidadeSecreta IdentidadeSecreta { get; set; }
+    public virtual ICollection<HeroiBatalha> Batalhas { get; set; }
+    public virtual ICollection<Arma> Armas { get; set; }
+}
